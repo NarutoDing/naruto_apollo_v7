48f6bc06a4bf52a3a259df6afc6d1ac3a7951bd5
jding <Jiangang.Ding@harman.com>
2022-04-17 11:51:49 +0800

    add cancard cmake

---
+++ cmake/cancard.cmake
@@ -0,0 +1,8 @@
+# Finds  cancard and compilers and extends
+
+include_directories($ENV{THIRD_LIBRARY_INSTALL_PATH}/can_card_library/hermes_can/include)
+file(GLOB hermes_can $ENV{THIRD_LIBRARY_INSTALL_PATH}/can_card_library/hermes_can/lib_x86_64/*.so)
+
+
+
+

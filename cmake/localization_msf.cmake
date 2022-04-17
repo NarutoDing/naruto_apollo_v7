d1ca143933a8d3f571d827915c81671712bbf6ba
jding <Jiangang.Ding@harman.com>
2022-04-17 11:18:49 +0800

    add smart eye

---
+++ cmake/localization_msf.cmake
@@ -0,0 +1,7 @@
+# Finds  json and compilers and extends
+include_directories($ENV{THIRD_LIBRARY_INSTALL_PATH}/localization_msf/x86_64/include)
+set(localization_msf $ENV{THIRD_LIBRARY_INSTALL_PATH}/localization_msf/x86_64/lib/liblocalization_msf.so)
+
+
+
+

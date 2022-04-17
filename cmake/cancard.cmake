356a1124eb5bf6aaccc32358b2d05e797be9068e
jding <Jiangang.Ding@harman.com>
2022-04-17 13:25:30 +0800

    fix bug

--- cmake/cancard.cmake
+++ cmake/cancard.cmake
@@ -1,8 +1,9 @@
 # Finds  cancard and compilers and extends
 
 include_directories($ENV{THIRD_LIBRARY_INSTALL_PATH}/can_card_library/hermes_can/include)
+include_directories($ENV{THIRD_LIBRARY_INSTALL_PATH}/can_card_library)
 file(GLOB hermes_can $ENV{THIRD_LIBRARY_INSTALL_PATH}/can_card_library/hermes_can/lib_x86_64/*.so)
-
+file(GLOB esd_can $ENV{THIRD_LIBRARY_INSTALL_PATH}/can_card_library/esd_can/libntcan.so.4.2.3)
 
 
 
